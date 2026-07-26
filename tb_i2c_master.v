ed951d7a420068ddec4115c0dec35be907a66462
Malak-Ibrahim193 <malakibrahim193@gmail.com>
2026-07-26 03:50:57 +0300

    Update tb_i2c_master.v

--- tb_i2c_master.v
+++ tb_i2c_master.v
@@ -9,12 +9,12 @@ module tb_i2c_master();
     reg        m_stop_i;
     reg  [6:0] m_slv_add_i;
     reg  [7:0] m_data_i;
-    reg        m_ack_i;
 
     wire [7:0] m_data_o;
     wire       m_busy_o;
     wire       m_error_o;
     wire       m_data_ready_o;
+    wire       m_ack_out_o;
 
     wire       sda;
     wire       scl;
@@ -35,11 +35,11 @@ module tb_i2c_master();
         .m_stop_i(m_stop_i),
         .m_slv_add_i(m_slv_add_i),
         .m_data_i(m_data_i),
-        .m_ack_i(m_ack_i),
         .m_data_o(m_data_o),
         .m_busy_o(m_busy_o),
         .m_error_o(m_error_o),
         .m_data_ready_o(m_data_ready_o),
+        .m_ack_out_o(m_ack_out_o),
         .sda(sda),
         .scl(scl)
     );
@@ -54,7 +54,6 @@ module tb_i2c_master();
         m_stop_i    = 0;
         m_slv_add_i = 7'h00;
         m_data_i    = 8'h00;
-        m_ack_i     = 0;
         drive_sda   = 0;
         sda_val     = 0;
 
@@ -62,7 +61,6 @@ module tb_i2c_master();
         rst_i = 0;
         #40;
 
-        // Write Operation 
         m_slv_add_i = 7'h3A;
         m_w_r_i     = 1'b0;
         m_data_i    = 8'hA5;
@@ -70,25 +68,19 @@ module tb_i2c_master();
         #20;
         m_start_i   = 1'b0;
 
-        // Drive Slave ACK on Address Phase
         #180;
         drive_sda = 1'b1; sda_val = 1'b0;
         #20;
         drive_sda = 1'b0;
 
-        // Drive Slave ACK on Data Phase
         #160;
-        drive_sda = 1'b1; 
         drive_sda = 1'b1; sda_val = 1'b0;
+        m_stop_i  = 1'b1;
         #20;
         drive_sda = 1'b0;
-
-        m_stop_i = 1'b1;
-        #40;
-        m_stop_i = 1'b0;
+        m_stop_i  = 1'b0;
 
         #200;
-        $display("DONE");
         $finish;
     end
 
